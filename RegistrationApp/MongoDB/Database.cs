4b278b2b589f052294133bece1b168650cefa51c
Радмир Габдрахманов <gabdrahmanov.radmir2018@gmail.com>
2022-10-22 11:12:47 +0300

    Добавьте файлы проекта.

---
+++ RegistrationApp/MongoDB/Database.cs
@@ -0,0 +1,38 @@
+﻿using global::MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Xml.Linq;
+using static MongoDB.Bson.Serialization.Serializers.SerializerHelper;
+
+namespace RegistrationApp.MongoDB
+{
+    static public class Database
+    {
+        public static void AddToDatabase(User user)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Users");
+            var collection = database.GetCollection<User>("Users");
+            collection.InsertOne(user);
+        }
+
+        public static User FindByName(string login, string password)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Users");
+            var collection = database.GetCollection<User>("Users");
+            var user = collection.Find(x => x.Login == login).FirstOrDefault();
+
+            if (user != null)
+            {
+                return null;
+            }
+            if (user.Password == password)
+            {
+                return user;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ RegistrationApp/MongoDB/User.cs
@@ -0,0 +1,76 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System.Xml.Linq;
+
+namespace RegistrationApp.MongoDB
+{
+    public class User
+    {
+
+        [BsonIgnoreIfDefault]
+        public Object _id;
+
+        [BsonIgnoreIfDefault]
+        private string fullName;
+
+        [BsonIgnoreIfDefault]
+        private string email;
+
+        [BsonIgnoreIfDefault]
+        private string login;
+
+        [BsonIgnoreIfDefault]
+        private string password;
+
+        [BsonIgnoreIfDefault]
+        public string Name
+        {
+            get
+            {
+                return fullName;    // возвращаем значение свойства
+            }
+            set
+            {
+                fullName = value;   // устанавливаем новое значение свойства
+            }
+        }
+
+        [BsonIgnoreIfDefault]
+        public string Login
+        {
+            get
+            {
+                return login;    // возвращаем значение свойства
+            }
+            set
+            {
+                login = value;   // устанавливаем новое значение свойства
+            }
+        }
+
+        [BsonIgnoreIfDefault]
+        public string Password
+        {
+            get
+            {
+                return password;    // возвращаем значение свойства
+            }
+            set
+            {
+                password = value;   // устанавливаем новое значение свойства
+            }
+        }
+
+        [BsonIgnoreIfDefault]
+        public string Email
+        {
+            get
+            {
+                return email;    // возвращаем значение свойства
+            }
+            set
+            {
+                email = value;   // устанавливаем новое значение свойства
+            }
+        }
+    }
+}
